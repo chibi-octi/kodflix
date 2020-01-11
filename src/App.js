cdb3b25e5006a50ddc7efa627a2e910a8f5e0d2f
Declan Talbert <declan_at@hotmail.com>
2020-01-11 12:10:18 +0000

    Added 6 film covers, 3 columns 2 rows

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import film1 from './image-train-busan.jpg';
+import film1 from './images/film-spirited.jpg';
+import film2 from './images/film-arrietty.jpg';
+import film3 from './images/film-nausicaa.jpg';
+import film4 from './images/film-mary.jpg';
+import film5 from './images/film-ponyo.jpg';
+import film6 from './images/film-whisper.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <img src={film1} />
-    </div>
+      <div className="container">
+        <div className="item">
+          <img src={film1} alt="Sprited away" />
+        </div>
+        <div className="item">
+          <img src={film2} alt="Arrietty" />
+        </div>
+        <div className="item">
+          <img src={film3} alt="Nausicaa" />
+        </div>
+        <div className="item">
+          <img src={film4} alt="Mary" />
+        </div>
+        <div className="item">
+          <img src={film5} alt="Ponyo" />
+        </div>
+        <div className="item">
+          <img src={film6} alt="Whisper" />
+        </div>
+      </div>
+  </div>
   );
 }
 
